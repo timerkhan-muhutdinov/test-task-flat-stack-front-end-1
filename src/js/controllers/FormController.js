debd0e094b3a4031b70bfa777943f564ad2d46a0
Timerhasn <timerking@rambler.ru>
2020-04-01 02:49:28 +0300

    Implemented a method for submitting a form and displaying

--- src/js/controllers/FormController.js
+++ src/js/controllers/FormController.js
@@ -4,11 +4,18 @@ class FormController {
       this._view = view;
 
       view.on('changeTab', this.changeTab.bind(this));
-      console.log("FormController"); 
-       
+      view.on('submitForm', this.submitForm.bind(this));
+
       view.handleNext();
     }
   
+    submitForm(data){
+        console.log("FormController submitForm");  
+        const request = this._model.postForm(data);
+
+       this._view.showSuccess(request);
+    }
+
     changeTab() {
       console.log("FormController changeTab");  
       const tab = this._model.nextTab();
--- src/js/views/FormView.js
+++ src/js/views/FormView.js
@@ -3,17 +3,53 @@ import { EventEmitter } from '../EventEmitter';
 class FormView extends EventEmitter {
     constructor() {
         super();
-        console.log("FormView");
+
+        this.form = document.forms.order;
         this.button = document.getElementById("button");
         this.tabs = document.getElementsByClassName("order-form__tab");
         this.multiSteps = document.getElementsByClassName("steps__item");
 
-        this.button.addEventListener('click', this.handleNext.bind(this));
+        console.log("FormView");
+
+        this.refHandlerNext = this.handleNext.bind(this);
+        this.button.addEventListener('click', this.refHandlerNext);
     }
 
     handleNext() {
-        console.log("FormView handleNext");
         this.emit('changeTab');
+    }
+
+    handleSubmit() {
+        let elements = this.form.elements;
+        const data = {
+            shipping: {
+                fullName: elements["shipping.fullName"].value,
+                phone: elements["shipping.phone"].value,
+                street: elements["shipping.street"].value,
+                address: elements["shipping.address"].value,
+                city: elements["shipping.city"].value,
+                country: elements["shipping.country"].value,
+                zip: elements["shipping.zip"].value,
+            },
+            billing: {
+                fullName: elements["billing.fullName"].value,
+                email: elements["billing.email"].value,
+                street: elements["billing.street"].value,
+                address: elements["billing.address"].value,
+                city: elements["billing.city"].value,
+                country: elements["billing.country"].value,
+                zip: elements["billing.zip"].value,
+            },
+            card: {
+                fullName: elements["card.fullName"].value,
+                number: elements["card.number"].value,
+                date: elements["card.date"].value,
+                cvc: elements["card.cvc"].value,
+                
+            }
+        }
+
+        this.emit('submitForm', data);
     }
 
     show(newTab) {
@@ -32,17 +68,28 @@ class FormView extends EventEmitter {
         }
 
         this.tabs[newTab].style.display = "block";
+
+        if((this.tabs.length - newTab) == 1)
+        {
+            this.button.innerHTML = "Pay Securely";
+            this.button.removeEventListener('click', this.refHandlerNext);
+            this.button.addEventListener('click', this.handleSubmit.bind(this));
+        }
     }
 
-    /*showChanges(event) {
-        this.input = event.target;
-        const obj = { value: this.input.value,
-            mask: this.input.dataset.mask};
+    showSuccess(data){
+        console.log("showSuccess", data);
+        this.form.style.display = "none";
+        //document.querySelector(".main__content .success").style.display = "block";
 
-      this.emit('changeItemMasc', obj);
+        document.querySelector(".main__content .success").innerHTML = `<h2>Thank you for your order!</h2>
+        <p><strong>Order number is: ${data.numberOrder} </strong></p>
+        <p>Your will recieve an email confirmation <br>
+            shortly to <a href="mailto:jonathan.smith@gmail.com">jonathan.smith@gmail.com</a></p>
+        <p>Estimated delivery Day is <br>
+            <b>${data.deliveryDate.toDateString()}</b></p>
+        <a href="">Print Recipe</a>`;
     }
-
-    */
 }
 
 export default FormView;
