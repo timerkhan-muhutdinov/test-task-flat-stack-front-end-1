8dbf6793e0b403740a1a8a8014662d99ef1dd40b
Timerhasn <timerking@rambler.ru>
2020-03-31 19:35:55 +0300

    Added geo methods

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,21 @@
 import  InputMaskModel from './models/InputMaskModel';
+import  GeoAutoCompleteModel from './models/GeoAutoCompleteModel';
 
 import  InputMaskView from './views/InputMaskView';
+import  GeoAutoCompleteView from './views/GeoAutoCompleteView';
 
 import  InputMaskController from './controllers/InputMaskController';
+import  GeoAutoCompleteController from './controllers/GeoAutoCompleteController';
 
 
 // initialize InputMask
 const  inputMaskModel = new InputMaskModel();
 const  inputMaskView = new InputMaskView();
 const  inputMaskController = new InputMaskController(inputMaskModel, inputMaskView);
+
+// initialize geoAutoComplete
+const  geoAutoCompleteModel = new GeoAutoCompleteModel();
+const  geoAutoCompleteView = new GeoAutoCompleteView();
+const  geoAutoCompleteController = new GeoAutoCompleteController(geoAutoCompleteModel, geoAutoCompleteView);
+
+
---
+++ src/js/views/GeoAutoCompleteView.js
@@ -0,0 +1,42 @@
+import { EventEmitter } from '../EventEmitter';
+
+class GeoAutoCompleteView extends EventEmitter {
+    constructor() {
+      super();
+
+      this.maskedInputs = document.querySelectorAll(".geo");
+        for (var index = 0; index < this.maskedInputs.length; index++) {
+            this.maskedInputs[index].addEventListener('input', this.onClickGeo.bind(this));
+        }
+    }
+  
+    onClickGeo() {
+        this.emit('autocomplete');
+    }
+
+    handleGeoAutocomplete(data) {
+        const inputCity = document.getElementById("city");
+        const inputZip = document.getElementById("zip");
+        const options = document.querySelectorAll('#countries > option');
+
+        options.forEach(function(item, i) {
+            if(item.value.indexOf(data.results[0].address_components[6].short_name) != -1)            
+                {
+                    item.selected = true;
+                }
+          });
+
+        inputCity.value = data.results[0].address_components[2].long_name;
+        inputZip.value = data.results[0].address_components[7].long_name;    
+    }
+
+    show(countries) {
+
+        const selector = document.getElementById('countries');
+
+        countries.forEach((item) => selector.append(new Option(item.name, item.code)));
+
+    }
+}
+
+export default GeoAutoCompleteView;
